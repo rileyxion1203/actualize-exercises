40dcae848bb3c65c7007c5d36862dd1be61bc0ac
rileyxion1203 <rileyxiong1203@gmail.com>
2024-09-23 17:30:32 -0400

    Create 112.rb

---
+++ 112.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write code to print hellohellohello using string addition.
+# Then write code to print hellohellohello again, this time using string multiplication.
+puts "hello"
+#without the code, Ruby thinks it as of a special code
+
+puts "hellohellohello" + "Kevin"
+puts "hello"*3
+
+
+
+# QUESTION 2
+# Read the code below and predict which lines will crash. 
+# Then run the code and remove the invalid lines one at a time, until only the valid code remains.
+puts 9 + 3
+puts "9" + "3"
+puts 9 - 3
+puts 9 * 3
+puts "9" * 3
+puts 9 / 3
