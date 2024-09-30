5411e7da99b575cd0caf0b0db7eb01abd8d87050
rileyxion1203 <rileyxiong1203@gmail.com>
2024-09-29 22:58:41 -0400

    Create 142.rb

---
+++ 142.rb
@@ -0,0 +1,25 @@
+# QUESTION 1
+# Write a program that starts with an array of strings, 
+# then prints out each string on separate lines using a while loop.
+
+strings = ["Cats","Dogs","Frogs","Elephants"]
+
+index = 0
+while index < strings.length
+    puts strings[index]
+    index = index + 1
+end
+
+# QUESTION 2
+# Write a program that starts with an array of numbers, 
+# then prints out each number times 3 using a while loop.
+
+numbers = [1, 13,16,22]
+
+index  = 0
+while index < numbers.length
+    puts numbers[index]*3
+    index = index + 1
+end
+
+ 
