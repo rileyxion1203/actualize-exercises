baccf1d48f1775c6dffe5a32dd92f80fe8cc5539
rileyxion1203 <rileyxiong1203@gmail.com>
2024-09-30 00:17:08 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,30 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def rileys_design
+    puts  "  __ _ _                          _                                             _            "
+    puts  "  /__(_) | ___ _   _  __      ____ _| | _____  ___   _   _ _ __     ___  __ _ _ __| |_   _   "
+    puts  " / \// | |/ _ \ | | | \ \ /\ / / _` | |/ / _ \/ __| | | | | '_ \   / _ \/ _` | '__| | | | |  "
+    puts  "/ _  \ | |  __/ |_| |  \ V  V / (_| |   <  __/\__ \ | |_| | |_) | |  __/ (_| | |  | | |_| |_ "
+    puts  "\/ \_/_|_|\___|\__, |   \_/\_/ \__,_|_|\_\___||___/  \__,_| .__/   \___|\__,_|_|  |_|\__, (_)"
+    puts  "                |___/                                      |_|                        |___/  "
+end
+
+rileys_design
+
+
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+
+print_lyrics
