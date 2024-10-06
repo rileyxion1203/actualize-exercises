e2a6e66d2da035f630938ca3c8bda6b614f64f72
Riley Xiong <rileyxiong1203@gmail.com>
2024-10-06 17:54:37 -0400

    Add practice problem

--- 204.rb
+++ 204.rb
@@ -4,5 +4,6 @@
 # Fill in the answers for each question in the Career Inventory.
 # Change the sharing settings of the document to "Anyone with the link can view" 
 # Copy the link to your Career Inventory Google document and paste it below.
+# Riley Xiong
 
 print "https://docs.google.com/document/d/1SugRHO0EYz5uPGypX8w8EyijjLKDJQik9jv52tvym0Y/edit#heading=h.gyzkhjev28mz"
